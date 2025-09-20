b5f1928a5e5c435e4a00543a2aa6c91d29aab319
atrobotics <ahsantanveer3883@gmail.com>
2025-09-20 15:07:25 +0000

    Deploying to gh-pages from @ atrobotics/atrobotics.github.io@cd25a6dc8fe539b065d78f438393d842c2689864 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,11 +53,6 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
@@ -68,11 +63,6 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
